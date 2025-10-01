606720c14f82138750c8dab9b495ff4dd8f9a56c
Gerardo Gabriel Caldas Diaz <gerardo.caldas@cruzdelsur.com.pe>
2025-10-01 09:34:31 -0500

    Nueva actualizacion

--- src/app/pages/header/header.ts
+++ src/app/pages/header/header.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, FontAwesomeModule],
   templateUrl: './header.html',
   styleUrls: ['./header.css']
 })
 export class Header {
   isMenuOpen: boolean = false;
+  faWhatsapp = faWhatsapp;
 
   toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
